449c1154f019801daff467565b2b3fbd128448c6
tiandi03 <tiandi03@kuaishou.com>
2020-05-29 21:22:57 +0800

    btree

--- tree/btree.go
+++ tree/btree.go
@@ -36,8 +36,17 @@ func (t *GenericBTree) Insert(v ds.Comparable) {
 		cur.insertKey(v)
 	} else {
 
+	}
+}
 
-
+func spill(v ds.Comparable, leaf *btnode, pstack []*btnode) {
+	cur := leaf
+	cur.insertKey(v)
+	for !cur.isFull() && len(pstack) != 0 {
+		silbling, key := cur.spilt()
+		cur = pstack[len(pstack)-1]
+		cur.insertKey(key)
+		cur.insertNode(silbling)
 	}
 }
 
@@ -51,7 +60,7 @@ type btnode struct {
 // t-1  <= len(keys) <= 2*t - 1
 func newbtnode(v ds.Comparable, tree *GenericBTree) *btnode {
 	node := &btnode{
-		keys:make([]ds.Comparable, 0, 2*tree.degree-1),
+		keys:make([]ds.Comparable, 0, 2*tree.degree), // capacity is 2*tree*degree so we pre-allocate an entry for spilled key
 		nodes:make([]*btnode, 0, 2*tree.degree),
 		tree:tree,
 	}
@@ -77,8 +86,27 @@ func(n *btnode) search(v ds.Comparable) int {
 	return len(n.keys)
 }
 
-// assume (1) n is nut full (2) n is a leaf node
 func (n *btnode) insertKey(v ds.Comparable) {
 	at := n.search(v)
 	copy(n.keys[at+1:], n.keys[at:])
+}
+
+func (n *btnode) insertNode(node *btnode) {
+
+}
+
+func(n *btnode) spilt() (*btnode, ds.Comparable) {
+	if !n.isFull() {
+		return nil, nil
+	}
+	mid := len(n.keys)/2
+	upkey := n.keys[mid]
+	sibling := &btnode{
+		keys: n.keys[mid+1:],
+		nodes: n.nodes[mid:],
+		tree: n.tree,
+	}
+	n.keys = n.keys[:mid+1]
+	n.nodes = n.nodes[:mid]
+	return sibling, upkey
 }
